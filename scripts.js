acaec8e19a4c918225796b0341067f11d9d5c114
Pawel Glowacki <eros.z@poczta.fm>
2017-01-08 14:21:16 +0100

    moved function

--- scripts.js
+++ scripts.js
@@ -3,13 +3,12 @@ function Telefon(marka, cena, kolor, warranty) {
     this.cena = cena;
     this.kolor = kolor;
    	this.warranty = getWarrantyCost(cena);
+   	function getWarrantyCost(cena) {
+		var x = cena * 0.1;
+   		var result = cena + x;
+   		return result;
+	} 
 }
-
-function getWarrantyCost(cena) {
-	var x = cena * 0.1;
-   	var result = cena + x;
-   	return result;
-} 
 
 Telefon.prototype.printInfo = function() {
      console.log('Marka telefonu to ' + this.marka + ', kolor to ' + this.kolor + ', a cena to ' + this.cena + '. ' +'Cena z przedłużoną gwarancją wynosi ' + this.warranty);
